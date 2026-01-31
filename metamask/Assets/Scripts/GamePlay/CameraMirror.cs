ed6e488eb9f4b945e774b3c4b4468ae8ef688e37
David Liebemann <david@unterpunkt9.com>
2026-01-31 17:09:51 +0100

    Current progress

--- metamask/Assets/Scripts/GamePlay/CameraMirror.cs
+++ metamask/Assets/Scripts/GamePlay/CameraMirror.cs
@@ -1,23 +1,50 @@
 using System;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 public class CameraMirror : MonoBehaviour
 {
+    [SerializeField] private LayerMask selectionLayer;
+    
     [SerializeField] private Camera handledCamera;
+    [SerializeField] private InputActionReference selectPressed;
+    [SerializeField] private InputActionReference selectionPosition;
+    
 
     public Camera HandledCamera => handledCamera;
     
     private void Awake()
     {
         Assert.IsNotNull(handledCamera);
+        Assert.IsNotNull(selectionPosition);
+        Assert.IsNotNull(selectPressed);
         handledCamera.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        selectPressed.action.Enable();
+        selectionPosition.action.Enable();
     }
 
 
     public void SetMirrorUsed(bool bShouldBeUsed)
     {
         handledCamera.gameObject.SetActive(bShouldBeUsed);
+    }
+
+    private void Update()
+    {
+        if (selectPressed.action.IsPressed())
+        {
+            Ray selectionRay = HandledCamera.ScreenPointToRay(selectionPosition.action.ReadValue<Vector2>());
+            if (Physics.Raycast(selectionRay, out var hit,  1000.0f, selectionLayer))
+            {
+                Debug.Log($"Selected: {hit.collider.transform.parent.gameObject.name}");
+            }
+        }
+       
     }
 
     private void OnMouseEnter()
