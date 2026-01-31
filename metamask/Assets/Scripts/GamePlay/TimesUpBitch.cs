59f05ac85f07be6e5dfac29a49cad5a541dfd6fa
brxk <mail@lindaunterberg.de>
2026-01-31 20:50:58 +0100

    Times Up Bitch
    Pocket Watch Timer

---
+++ metamask/Assets/Scripts/GamePlay/TimesUpBitch.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimesUpBitch : MonoBehaviour {
+    public void EndGame() {
+        Debug.Log("Time's up! Unfortunately your Prince Charming is gone.");
+    }
+
+}
