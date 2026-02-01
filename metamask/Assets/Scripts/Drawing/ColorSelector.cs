87bafb31ade911d2dec75c3ac67ca061f595a217
David Liebemann <david@unterpunkt9.com>
2026-02-01 14:01:22 +0100

    Selection of colors

---
+++ metamask/Assets/Scripts/Drawing/ColorSelector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using GamePlay;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Drawing
+{
+    [RequireComponent(typeof(Collider), typeof(Outline))]
+    public class ColorSelector : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Color color;
+
+        public Color DrawColor => color;
+
+        private Outline _outline;
+        private void Awake()
+        {
+            _outline = GetComponent<Outline>();
+            Assert.IsNotNull(_outline);
+        }
+
+        private void OnEnable()
+        {
+            _outline.enabled = false;
+        }
+
+
+        public void OnSelect(GameObject originator, bool bIsSelected)
+        {
+            if (bIsSelected)
+            {
+                _outline.OutlineColor = Color.lawnGreen;
+                _outline.OutlineWidth = 10.0f;
+            }
+            else
+            {
+                _outline.OutlineColor = Color.yellow;
+                _outline.OutlineWidth = 5.0f;
+            }
+        }
+
+        public void OnHover(GameObject originator, bool bIsHovered)
+        {
+            _outline.enabled = bIsHovered;
+        }
+    }
+}
---
+++ metamask/Assets/Scripts/Drawing/HoverSelectionBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using GamePlay;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Drawing
+{
+    public class HoverSelectionBehaviour : MonoBehaviour
+    {
+
+        [SerializeField] private InputActionReference selectPressed;
+        [SerializeField] private InputActionReference selectionPosition;
+        [SerializeField] private LayerMask selectionLayer;
+
+        private IInteractable _currentlyHovered;
+
+        private void OnEnable()
+        {
+            selectPressed.action.Enable();
+            selectionPosition.action.Enable();
+            
+        }
+
+        private void Update()
+        {
+            if (Camera.allCamerasCount > 0)
+            {
+                Camera currentCamera = Camera.allCameras[0];
+                if (currentCamera)
+                {
+                    Ray selectionRay =
+                        currentCamera.ScreenPointToRay(selectionPosition.action.ReadValue<Vector2>());
+                    if (Physics.Raycast(selectionRay, out var hit, 1000.0f, selectionLayer))
+                    {
+                        IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+                        if (interactable != _currentlyHovered)
+                        {
+                            SetCurrentHover(false);
+                        }
+
+                        _currentlyHovered = interactable;
+                        if (null != _currentlyHovered)
+                        {
+                            SetCurrentHover(true);
+                            if (selectPressed.action.IsPressed())
+                            {
+                                SetCurrentSelect(true);
+                            }
+                            else
+                            {
+                                SetCurrentSelect(false);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        SetCurrentHover(false);
+                        SetCurrentSelect(false);
+                    }
+                }
+            }
+        
+        }
+
+        private void SetCurrentSelect(bool newValue)
+        {
+            if (null != _currentlyHovered)
+            {
+                _currentlyHovered.OnSelect(gameObject, newValue);
+            }
+        }
+
+        private void SetCurrentHover(bool bNewValue)
+        {
+            if (null != _currentlyHovered)
+            {
+                _currentlyHovered.OnHover(gameObject, bNewValue);
+                if (!bNewValue)
+                {
+                    SetCurrentSelect(false);
+                }
+            }
+        }
+    }
+}
