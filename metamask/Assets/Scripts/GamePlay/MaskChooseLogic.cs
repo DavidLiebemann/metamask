b73ea5af4d24b453bd1ef360d27fbd993d591c03
David Liebemann <david@unterpunkt9.com>
2026-02-01 13:09:43 +0100

    Can now switch masks

---
+++ metamask/Assets/Scripts/GamePlay/MaskChooseLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GamePlay
+{
+    public class MaskChooseLogic : MonoBehaviour
+    {
+
+        private GameObject[] _masks;
+        private int _activeMaskId = 0;
+
+        private void OnEnable()
+        {
+            _masks = new GameObject[transform.childCount];
+            _activeMaskId = Random.Range(0, _masks.Length);
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                _masks[i] = transform.GetChild(i).gameObject;
+            }
+
+            UpdateMasksActive();
+        }
+
+        public void UpdateMasksActive()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                _masks[i].SetActive(i == _activeMaskId);
+            }
+        }
+
+
+        public void NextMask()
+        {
+            _activeMaskId = (_activeMaskId + 1) % _masks.Length;
+            UpdateMasksActive();
+        }
+
+        public void PreviousMask()
+        {
+            _activeMaskId = (_activeMaskId - 1) % _masks.Length;
+            if (_activeMaskId < 0)
+            {
+                _activeMaskId += _masks.Length;
+            }
+            UpdateMasksActive();
+        }
+    }
+}
