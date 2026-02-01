7ae06af884246313afcbe4bcd13d0d7b8fd883d7
David Liebemann <david@unterpunkt9.com>
2026-02-01 17:48:19 +0100

    New scale

---
+++ metamask/Assets/Scripts/GamePlay/ClickSound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GamePlay
+{
+    public class ClickSound : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference click;
+        [SerializeField] private AudioClip toPlay;
+        [SerializeField] private AudioSource player;
+
+        private void OnEnable()
+        {
+            click.action.Enable();
+        }
+
+  
+
+        private void Update()
+        {
+            if (click.action.WasPressedThisFrame())
+            {
+                player.PlayOneShot(toPlay);
+
+            }
+        }
+    }
+}
